52fdccc5bf1549766a647f0e12189d268764582c
Abdo Elkateb <elkatebdev@gmail.com>
2023-04-29 18:15:00 +0200

    edit file fron css and json

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 //  start js 
 
+// ASO 
+AOS.init({
+    duration:1500
+})
+
 //  control nevber & makes it fixed using  -- scroll 
 
 var main = document.getElementById("nav__main");
@@ -15,83 +20,89 @@ window.onscroll =  () => {
 
 };
 
+
 //  when you click the image it show you the big image,
 //  allow you to press the right, left, and close the image
 
-var avtive_box = document.getElementById("avtive_box");
-var avtive_box_item = document.getElementById("avtive_box_item");
-var right = document.getElementById("right")
-var left = document.getElementById("left")
-var close = document.getElementById("close");
-var imgs = Array.from(document.getElementsByClassName("image-click"));
-var imagsIndex = 0;
+// var avtive_box = document.getElementById("avtive_box");
+// var avtive_box_item = document.getElementById("avtive_box_item");
+// var right = document.getElementById("right")
+// var left = document.getElementById("left")
+// var close = document.getElementById("close");
+// var imgs = Array.from(document.getElementsByClassName("image-click"));
+// var imagsIndex = 0;
 
-// click image 
-for(var i = 0; i < imgs.length; i++) {
+// // click image 
 
-  imgs[i].addEventListener("click", (eventInfo) => {
-  avtive_box.style.display = "flex"
-  // click to get src image 
+// for(var i = 0; i < imgs.length; i++) {
 
-  // this is a  number index from images, click not right or left
-  imagsIndex = imgs.indexOf(eventInfo.target)
-  console.log(imagsIndex)
-  var imageSrc = eventInfo.target.src
-  avtive_box_item.style.backgroundImage = `url(${imageSrc})`
+//   imgs[i].addEventListener("click", (eventInfo) => {
+//   avtive_box.style.display = "flex"
+//   // click to get src image 
+//   console.log("its working")
+
+//   // this is a  number index from images, click not right or left
+//   imagsIndex = imgs.indexOf(eventInfo.target)
+//   console.log(imagsIndex)
+//   var imageSrc = eventInfo.target.src
+//   avtive_box_item.style.backgroundImage = `url(${imageSrc})`
+//   })
+// }
+
+
+// // close  image 
+// close.addEventListener("click", () => {
+//     avtive_box.style.display = "none"
+
+// })
+
+// // next image 
+// right.addEventListener("click", () => {
+//  imagsIndex++
+// //  console.log(imgs[imagsIndex].src);
+// if(imagsIndex == imgs.length) {
+//    imagsIndex = 0
+// }
+//  imageSrc = imgs[imagsIndex].src
+//   avtive_box_item.style.backgroundImage = `url(${imageSrc})`;
+
+// })
+// left.addEventListener("click", () => {
+//   imagsIndex++;
+//   //  console.log(imgs[imagsIndex].src);
+//   if (imagsIndex == imgs.length) {
+//     imagsIndex = 0;
+//   }
+//   imageSrc = imgs[imagsIndex].src
+//   avtive_box_item.style.backgroundImage = `url(${imageSrc})`;
+
+// });
+
+// document.addEventListener("click", () => {
+
   
-  })
-}
-
-
-// close  image 
-close.addEventListener("click", () => {
-    avtive_box.style.display = "none"
-
-})
-
-// next image 
-right.addEventListener("click", () => {
- imagsIndex++
-//  console.log(imgs[imagsIndex].src);
-if(imagsIndex == imgs.length) {
-   imagsIndex = 0
-}
- imageSrc = imgs[imagsIndex].src
-  avtive_box_item.style.backgroundImage = `url(${imageSrc})`;
-
-})
-left.addEventListener("click", () => {
-  imagsIndex++;
-  //  console.log(imgs[imagsIndex].src);
-  if (imagsIndex == imgs.length) {
-    imagsIndex = 0;
-  }
-  imageSrc = imgs[imagsIndex].src
-  avtive_box_item.style.backgroundImage = `url(${imageSrc})`;
-
-});
-
-document.addEventListener("click", () => {
-
-  
-});
+// });
 
 
 
 let card = new XMLHttpRequest()
 
-card.open("get", "work.json", true)
+card.open(
+  'get', '../js/card.json', true
+);
 
 card.send();
 
-card.onload = function() {
-  if(this.readyState == 4 && this.status == 200) {
-
+card.onreadystatechange = function () {
+  if(this.readyState === 4 && this.status === 200) {
+    console.log(this.responseText)
   }
 }
 
 
-// ASO 
-AOS.init({
-    duration:1500
-})
+
+
+
+
+
+console.log(URL.card)
